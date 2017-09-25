34aa66b6be589bb48d30034172638518dad9e49e
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2017-09-25 10:10:46 +0100

    convert comment descriptions to file docstring
    For more guidance, see PEP257 (https://www.python.org/dev/peps/pep-0257/),
    PEP8 (https://www.python.org/dev/peps/pep-0008/) and the following:
    
    https://stackoverflow.com/questions/2557110/what-to-put-in-a-python-module-docstring

--- bin/cazy_uniprot_dickeya.py
+++ bin/cazy_uniprot_dickeya.py
@@ -1,46 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103
-#automation.py
-#
-# (c) The James Hutton Institute 2016
-#Author: Eirini Xemantilotou
+"""cazy_uniprot_dickeya.py
 
-# This script does the following:
-# [1] Retrieve UniProt Dickeya entries with a cross-reference to CAZy
-#       Retrieves data in tabular format:
-#       accession  CAZy_xrefs
-#       Example:
-#       P0C1A7	PL9;
-#       P07103	CBM5;GH5;
-# [2] Create a dictionary mapping CAZy families to accessions
-#       e.g.
-#       {GH3: [P11073, D2BXL2, ...], ...}
-#       We use a default dict for this. As `caz` is just a string we use
-#       io.StringIO to handle it like a file whose lines we iterate over.
-#       StringIO reads and writes strings as files.
-#       As the first line contains the column headers, we ignore it.
-#       Each line is split into accession and a list of CAZy families; from
-#       this we construct the dictionary.
-# [3] For each CAZy family, retrieve the mapped UniProtKB accessions in fasta
-#       format and write to a file with name <CAZy_family>.fasta. My simple
-#       approach here retrieves each batch of sequences from uniprot.org. One
-#       could also download all Dickeya sequences in one go and then get them
-#       from that file. The bioservices package also does not provide the
-#       batch download facility that the UniProt API has which could
-#       be used here.
-#
-"""
+(c) The James Hutton Institute 2016
+Author: Eirini Xemantilotou
+
+This script does the following:
+[1] Retrieve UniProt Dickeya entries with a cross-reference to CAZy
+       Retrieves data in tabular format:
+       accession  CAZy_xrefs
+       Example:
+       P0C1A7	PL9;
+       P07103	CBM5;GH5;
+[2] Create a dictionary mapping CAZy families to accessions
+       e.g.
+       {GH3: [P11073, D2BXL2, ...], ...}
+       We use a default dict for this. As `caz` is just a string we use
+       io.StringIO to handle it like a file whose lines we iterate over.
+       StringIO reads and writes strings as files.
+       As the first line contains the column headers, we ignore it.
+       Each line is split into accession and a list of CAZy families; from
+       this we construct the dictionary.
+[3] For each CAZy family, retrieve the mapped UniProtKB accessions in fasta
+       format and write to a file with name <CAZy_family>.fasta. My simple
+       approach here retrieves each batch of sequences from uniprot.org. One
+       could also download all Dickeya sequences in one go and then get them
+       from that file. The bioservices package also does not provide the
+       batch download facility that the UniProt API has which could
+       be used here.
+
 Python script for getting sequences in FASTA format out from Uniprot, where
 the taxonomy filter is `Dickeya` and the entry has a CAZy database cross-
 reference.
 
 Sequences are written to FASTA files named by the corresponding CAZy family
 """
+
 import os
 import shutil
+
 from io import StringIO
 from collections import defaultdict
+
 import bioservices
 
 
@@ -57,6 +59,7 @@ caz_map = defaultdict(list)
 caz_file = StringIO(caz)
 caz_iter = iter(caz_file)
 next(caz_iter)  # this takes the iterator to line two
+
 for line in caz_iter:
     acc, xref = line.split()
     fams = xref.split(';')[:-1]
